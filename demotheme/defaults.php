903a68d3013b11735a8701aa98f43715fdc4ac06
Jon <jonathant@tlmint.org>
2015-01-12 16:51:42 +0000

    Changes to be committed: 	modified:   demotheme/core/css/header.css 	new file:   demotheme/core/img/demoCloud.svg 	new file:   demotheme/defaults.php 	modified:   demotheme/settings/personal.php
    .header.css modified to use demoCloud.svg file as login logo
    .demoCloud SVG logo added at no expense spared!
    .defaults.php now gives you whiteboxing options!
    .personal.php still does nothing!

---
+++ demotheme/defaults.php
@@ -0,0 +1,37 @@
+ <?php
+    /**
+    * See lib/private/defaults.php for a list of possible defaults
+    */
+ class OC_Theme {
+    public function getName() {
+		/* short name, used when referring to the software
+		* @return string
+		*/
+		return 'demoCloud';
+    }
+	public function getTitle() { 
+		/* can be a longer name, for titles
+		* @return string
+		*/
+		return 'demoCloud gives you wings! ';
+    }
+    public function getEntity() {
+		/* e.g. company name, used for footers and copyright notices
+		* @return string
+		*/
+		return 'demoCloud web';
+    }    
+    public function getSlogan() {
+		/* e.g. Strapline, used for footers and copyright notices
+		* @return string
+		*/
+		return 'providing half-baked examples since 2015';
+    }
+    public function getBaseUrl() {
+		/**
+		* get base URL for the organisation behind your ownCloud instance
+		* @return string
+		*/
+		return 'http://owncloud.org';
+    }
+}
--- demotheme/settings/personal.php
+++ demotheme/settings/personal.php
@@ -1,4 +1,10 @@
+<?php
+/**
+* This is an example template that overrides the default one.
+* Provided FREE of copyright, support and generally disowned by the
+* creator. Use at your own peril.
+*/
 
-this is an example template that overrides the default one
+// No idea what you'd put here, to be honest
 
-
+// EOF
